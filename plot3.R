7382ec65067b49fcb4bcef9c04c4e502e6685f88
johndeere19 <cwacker.caaax1@gmail.com>
2014-05-11 19:30:40 -0400

    JD19

--- plot3.R
+++ plot3.R
@@ -17,7 +17,7 @@ png("plot3.png", width = 480, height = 480)
 plot(data$fixdates, data$Sub_metering_1, type = "l", xlab="", ylab="Energy sub metering", col = "black")
 lines(data$fixdates, data$Sub_metering_2, type = "l", col = "red")
 lines(data$fixdates, data$Sub_metering_3, type = "l", col = "blue")
-legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1, lwd = 1, col= c("black", "red", "blue"), cex = 0.7)
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1, lwd = 1, col= c("black", "red", "blue"))
 
 ## end device
 dev.off()
