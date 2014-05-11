b154b6ac4a28c10b28470bc0f45a6f2896dbfd52
johndeere19 <cwacker.caaax1@gmail.com>
2014-05-11 19:20:29 -0400

    JD19

--- plot3.R
+++ plot3.R
@@ -14,7 +14,7 @@ data <- subset(data, data$Date == "2007-02-01" | data$Date == "2007-02-02")
 png("plot3.png", width = 504, height = 504)
 
 ## plot the days of week on xaxis and energy sub metering on y, lines
-plot(data$fixdates, data$Sub_metering_1, type = "l", xlab="", ylab="Energy Sub Metering", col = "black")
+plot(data$fixdates, data$Sub_metering_1, type = "l", xlab="", ylab="Energy sub metering", col = "black")
 lines(data$fixdates, data$Sub_metering_2, type = "l", col = "red")
 lines(data$fixdates, data$Sub_metering_3, type = "l", col = "blue")
 legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1, lwd = 1, col= c("black", "red", "blue"), cex = 0.7)
--- plot4.R
+++ plot4.R
@@ -10,24 +10,26 @@ data$fixdates <- strptime(data$fixdates, "%d/%m/%Y %T")
 ## subset out only the relevant data
 data <- subset(data, data$Date == "2007-02-01" | data$Date == "2007-02-02")
 
-## subset sub meterings
-subs <- c(, data$Sub_metering_2, data$Sub_metering_3)
-colorvec <- c("black", "red", "blue")
-
-
-
-
 ## open png graphics device
 png("plot4.png", width = 504, height = 504)
 
 ## paramters 2 row, 2 columns
+par(mfrow=c(2,2))
 
+## global active power kwatts
+plot(data$fixdates, data$Global_active_power, type = "l", xlab="", ylab="Global Active Power")
+
+## voltage 
+plot(data$fixdates, data$Voltage, type = "l", xlab="datetime", ylab="Voltage")
 
 ## plot the days of week on xaxis and energy sub metering on y, lines
-plot(data$fixdates, data$Sub_metering_1, type = "l", xlab="", ylab="Energy Sub Metering", col = "black")
+plot(data$fixdates, data$Sub_metering_1, type = "l", xlab="", ylab="Energy sub metering", col = "black")
 lines(data$fixdates, data$Sub_metering_2, type = "l", col = "red")
 lines(data$fixdates, data$Sub_metering_3, type = "l", col = "blue")
-legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1, lwd = 1, col= c("black", "red", "blue"), cex = 0.7)
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1, lwd = 1, col= c("black", "red", "blue"), cex = 0.7, bty = "n")
+
+## 
+plot(data$fixdates, data$Global_reactive_power, type = "l", xlab="datetime", ylab="Global_reactive_power")
 
 ## end device
 dev.off()
